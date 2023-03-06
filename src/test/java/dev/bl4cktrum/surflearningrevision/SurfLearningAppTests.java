4166501cc4768de579092fb4d13be99f40d10278
Yavuzhan Çaşkurlu <yavuzhancaskurlu@gmail.com>
2023-03-06 11:01:06 +0300

    chore: initial commit

---
+++ src/test/java/dev/bl4cktrum/surflearningrevision/SurfLearningAppTests.java
@@ -0,0 +1,13 @@
+package dev.bl4cktrum.surflearningrevision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfLearningAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
